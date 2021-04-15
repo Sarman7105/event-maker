d1649bc03b5e90109d06a697b64ee36375dcb9fe
Sarman7105 <sarman7105@gmail.com>
2021-04-15 11:22:30 +0600

    first commit

---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCPdu3WKio9wsNO1hQ0OvvXOyInPzI-VBA",
+  authDomain: "event-maker-a7d35.firebaseapp.com",
+  projectId: "event-maker-a7d35",
+  storageBucket: "event-maker-a7d35.appspot.com",
+  messagingSenderId: "618412161724",
+  appId: "1:618412161724:web:d0b6ef3607b6538a6baeb9"
+};
+export default firebaseConfig;
